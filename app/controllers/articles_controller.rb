ee1c5e859be082cf118f322509d8a424d62fd083
lewisky <lewisky28mawira@gmail.com>
2023-07-01 16:50:32 +0300

    completed assignment

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -17,4 +17,19 @@ class ArticlesController < ApplicationController
     render json: { error: "Article not found" }, status: :not_found
   end
 
+  def show
+    session[:page_views] ||= 0
+    session[:page_views] += 1
+
+    if session[:page_views] < 3
+      # Render the JSON response with the article data
+      article = Article.find(params[:id])
+      render json: article
+    else
+      # Render the JSON response with the error message and status code 401
+      render json: { error: "Unauthorized - Maximum page views reached" }, status: :unauthorized
+    end
+  end
+
+
 end
